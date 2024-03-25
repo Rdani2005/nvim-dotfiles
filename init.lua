d53f3d43e021588b62567517ed19e200e03ba2bf
Rdani2005 <daniel.sequeira@betanysports.eu>
2024-03-25 07:41:23 -0600

    Initial commit

---
+++ init.lua
@@ -0,0 +1,21 @@
+require("rdani2005.plugins-setup") -- plugins setup
+-- core
+require("rdani2005.core.options")
+require("rdani2005.core.keymaps")
+require("rdani2005.core.colorscheme")
+-- plugins
+require("rdani2005.plugins.comment")
+require("rdani2005.plugins.nvim-tree")
+require("rdani2005.plugins.lualine")
+require("rdani2005.plugins.telescope")
+require("rdani2005.plugins.nvim-cmp")
+require("rdani2005.plugins.autopairs")
+require("rdani2005.plugins.gitsigns")
+require("rdani2005.plugins.treesitter")
+
+-- plugins lsp
+require("rdani2005.plugins.lsp.lspconfig")
+require("rdani2005.plugins.lsp.lspsaga")
+require("rdani2005.plugins.lsp.mason")
+require("rdani2005.plugins.lsp.null-ls")
+
